1f260dafc1b7fe76e40432989e2a6bc379271de6
jporras123 <jporras@itba.edu.ar>
2022-05-06 21:38:37 -0300

    track

---
+++ src/Track.py
@@ -0,0 +1,10 @@
+
+
+class track:
+    def __init__(self):
+        self.notes = []
+        self.velocity = 127
+        self.activate = True
+        self.intrument = None
+        self.signal_out= []
+
