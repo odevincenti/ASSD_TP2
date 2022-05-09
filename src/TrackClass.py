7b072c2c235fa357618e714e3e8bfb8e49696bb7
odevincenti <odevincenti@outlook.com>
2022-05-09 01:22:25 -0300

    Actualizo update de TrackClass.py

--- src/TrackClass.py
+++ src/TrackClass.py
@@ -1,22 +1,19 @@
 class Track:
-    def __init__(self, notes=None, velocity=1, instrument=None):
+    def __init__(self, notes=None, velocity=127, instrument=None):
         self.notes = notes
         self.velocity = velocity
         self.activate = True
-        self.instrument = instrument      # 0=guitarra , 1= violin, 2= piano, 3= lo que quieran agregar
+        self.instrument = instrument      # 0 = guitarra , 1 = violín, 2 = piano, 3 = lo que quieran agregar
         self.signal_out = []
-        self.change = True      # Si esta en True es pq en el update se modificaron las notas/velocidad/instrument
+        self.change = False      # Si esta en True es pq en el update se modificaron las notas/velocidad/instrument
 
-    def update(self, newtrack):
-        if self.notes != newtrack.notes or self.velocity != newtrack.velocity or self.signal_out != newtrack.intrument:
-            self.notes = newtrack.notes
-            self.velocity = newtrack.velocity
-            self.signal_out = newtrack.intrument
-            self.activate = newtrack.activate
+    def update(self, velocity, activate, instrument):
+        if self.velocity != velocity or self.instrument != instrument:
+            self.velocity = velocity
+            self.instrument = instrument
+            self.activate = activate
             self.signal_out = []
-            self.change = 1
+            self.change = True
         else:
-            self.activate = newtrack.activate
-            self.change = 0
-
-
+            self.activate = activate
+            self.change = False
