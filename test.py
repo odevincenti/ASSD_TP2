d3af8a9d499bfa08c8eece0dd7ae358925cc4a80
Ine Qui <inequihillalt@gmail.com>
2022-05-09 01:33:43 -0300

    refactor

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ print("test")
 
 Parciales_Flauta = ProcessedNote()
 
-Parciales_Flauta.create_partial("./MATLAB/Parciales_txts/Parciales_Flauta_DO.txt",1,600)  #1 es flauta ponele
+Parciales_Flauta.create_partial("./MATLAB/Parciales_txts/Parciales_DO.txt",1,600)  #1 es flauta ponele
 
 
 
