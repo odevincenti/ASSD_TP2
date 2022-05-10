a3f2cd7cd8f23aeac9e9568d39acfb37a8406c86
valen <vvenier@itba.edu.ar>
2022-05-10 00:20:41 -0300

    se ponen bien las imagenes en el menu, salvo las notas

--- Frontend/front.py
+++ Frontend/front.py
@@ -1,9 +1,11 @@
 from Frontend.scr.menu import MenuWindow
 from PyQt5.QtWidgets import QApplication
-
+import scr.ui.nota
+import scr.ui.play
+import scr.ui.pause
+import scr.ui.stop
 
 if __name__ == '__main__':
-
 
     app = QApplication([])
     window = MenuWindow()
