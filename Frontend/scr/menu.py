00512ec4d4dd00a9b3e378bb04bf1db72e388971
valen <vvenier@itba.edu.ar>
2022-05-10 18:17:25 -0300

    Cree el counter para el slider de musica, ANDA!

--- Frontend/scr/menu.py
+++ Frontend/scr/menu.py
@@ -1,7 +1,8 @@
 import numpy as np
 import sys
 from Frontend.scr.track import TrackWidget
-from src.backend import *
+from Frontend.counter import Counter
+#from src.backend import *
 from PyQt5.QtWidgets import *
 from Frontend.scr.ui.menu import Ui_Form
 
@@ -11,8 +12,10 @@ class MenuWindow (QWidget, Ui_Form):
         super().__init__(*args, **kwargs)
         self.setupUi(self)
 
-        self.back = backend()
+        #self.back = backend()
+        self.counter = Counter(self, 20)
         self.track_array = []
+        self.first_time = 0
 
         self.label_track0.hide()
         self.pushButton_instrument_track0.hide()
@@ -37,7 +40,7 @@ class MenuWindow (QWidget, Ui_Form):
         filename = QFileDialog.getOpenFileNames()
         self.path = filename[0][0]
         print(self.path)
-        self.back.update_path(self.path)
+        #self.back.update_path(self.path)
         self.ammount_of_tracks = self.back.quantity_of_tracks()
         print("cantidad de tracks:")
         print(self.ammount_of_tracks)
@@ -48,28 +51,38 @@ class MenuWindow (QWidget, Ui_Form):
             self.track_array.append(self.aux_track)
             self.Track_Widget.layout().addWidget(self.aux_track)
 
+        self.horizontalSlider_Track.setMaximum(200)
+        print(self.horizontalSlider_Track.value())
 
     def save_file(self):
         print("save")
-        self.back.save_wav_file(self.path)
+        #self.back.save_wav_file(self.path)
 
     def sintetizar(self):
         print("sintetizar")
-        self.back.process_song()
+        #self.back.process_song()
+
 
     def graficar_espectrograma(self):
         print("graficar espectrograma")
 
 
     def play_song(self):
-        print("pone play maestro")
-        self.back.play_song()
+        print("PLAY")
+        if self.first_time == 0:
+            self.counter.start_thread()
+            self.first_time = 1
+        self.counter.pause_loop = False
+        #self.back.play_song()
 
 
     def pause_song(self):
-        print("para la motoneta rey")
-        self.back.pause_reproduction()
+        print("PAUSA")
+        self.counter.pause_loop = True
+        #self.back.pause_reproduction()
 
 
     def reset_song(self):
-        print("ponela de vuelta que es un temazo")
+        print("RESET")
+        self.counter.reset_loop = True
+        self.counter.start()
