d9a35d5978d1d3fe2b8849178290949435356f54
Ignacio Cutignola <cutignolai@gmail.com>
2022-05-10 00:09:02 -0300

    Se realiza prueba de Synth

--- src/Test_Synth.py
+++ src/Test_Synth.py
@@ -6,13 +6,14 @@ note = 59
 start_time = 4133521.625
 end_time = 5454544.0
 duration = 1321022.375
-fs = 1000
 freq = 245.94165062674196
 velocity = 95
-
+fs = 10000
 nota = Note(note, start_time, duration, end_time, velocity, fs)
 
-ProcessedNote.create_note(nota.note, 'F')
+Synth = ProcessedNote()
+
+Synth.create_note(nota, "F")
 
 time = nota.create_time_base()
 
